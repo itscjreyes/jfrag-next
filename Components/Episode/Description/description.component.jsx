f2c60b43d2d2bece0822de6e0592d15bd04e90ba
CJ Reyes <itscjreyes@gmail.com>
2020-08-12 16:40:41 -0400

    Podcast episode headings

--- Components/Episode/Description/description.component.jsx
+++ Components/Episode/Description/description.component.jsx
@@ -1,12 +1,13 @@
 export const EpDesc = ({description, transcript}) => (
     <div className="ep-desc">
+        <h3 className="section-heading">In This Episode</h3>
         <div dangerouslySetInnerHTML={{ __html: description }} />
         {
             transcript &&
-            <>
-            <h3 className="transcript-heading">Transcript</h3>
+            <div className="transcript">
+            <h3 className="section-heading">Transcript</h3>
             <div dangerouslySetInnerHTML={{ __html: transcript }} />
-            </>
+            </div>
         }
     </div>
 )
