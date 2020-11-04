2cdcdb38149a65c3a9342efa7ff1f09bcdc5bf6d
CJ Reyes <itscjreyes@gmail.com>
2020-11-04 15:34:06 -0500

    Added fb pixel using next-fbq

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/global.scss';
 import "easymde/dist/easymde.min.css";
 import 'react-quill/dist/quill.snow.css';
+import Router from "next/router";
+import withFBQ from "next-fbq";
 
-export default function App({ Component, pageProps }) {
+function App({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
+
+export default withFBQ("796485467802251", Router)(App);
