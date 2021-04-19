edae03e2c37029cfecdb278b2f518662d8f9fb36
CJ Reyes <itscjreyes@gmail.com>
2021-04-19 10:49:34 -0400

    Updated link in podcast section on homepage

--- Components/Podcast/podcast.component.jsx
+++ Components/Podcast/podcast.component.jsx
@@ -14,7 +14,7 @@ function Podcast(){
       <iframe title="The Working Capital Real Estate Podcast" src="///html5-player.libsyn.com/embed/destination/id/1789700/height/360/theme/custom/thumbnail/yes/direction/backward/no-cache/true/render-playlist/yes/custom-color/87A93A/" height="auto" width="100%" scrolling="yes"  allowFullScreen webkitallowfullscreen="true" mozallowfullscreen="true" oallowfullscreen="true" msallowfullscreen="true"></iframe>
 
       <div className="btn-center">
-        <Link href="/podcast"><a className="btn btn-hollow">All Episodes</a></Link>
+        <a href="https://cadenceinvesting.com/podcast/" className="btn btn-hollow">All Episodes</a>
       </div>
     </div>
   )
